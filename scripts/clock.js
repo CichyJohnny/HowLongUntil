d481b2e02677494c9d19edddef066bbcd9e27446
Jan Cichy <04cichy@gmail.com>
2024-07-23 21:20:28 +0200

    restructure and new doc

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,8 +1,12 @@
 let targetDate;
 let countdownInterval;
 
+// Global variable with last checked day, hour, minute and second for comparison
 let prev_days, prev_hours, prev_minutes, prev_seconds;
 
+/**
+ * @description Show clock counting down
+ */
 function startCountdown() {
     const yearSelect = document.getElementById('year-select').value;
     const monthSelect = document.getElementById('month-select').value;
@@ -12,14 +16,16 @@ function startCountdown() {
     
     targetDate = new Date(yearSelect, monthSelect, daySelect, hourSelect, minuteSelect);
 
-    document.getElementsByClassName('container1')[0].style.visibility = 'visible';
-    document.getElementsByClassName('container2')[0].remove();
+    // Hide input, show clock
+    document.getElementsByClassName('clock-container')[0].style.visibility = 'visible';
+    document.getElementsByClassName('input-container')[0].remove();
 
     const now = new Date();
     const diff = targetDate - now;
-    
+
+    // Get days, hours, minutes and seconds of difference
     const d = Math.floor(diff / 86400000);
-    const h = Math.floor(diff / 3600000);
+    const h = Math.floor((diff % 86400000) / 3600000);
     const m = Math.floor((diff % 3600000) / 60000);
     const s = Math.floor((diff % 60000) / 1000);
 
@@ -28,14 +34,26 @@ function startCountdown() {
     prev_minutes = m;
     prev_seconds = s;
 
+    // Init clock with start time
     init_clock_digits(prev_days, prev_hours, prev_minutes, prev_seconds);
+
+    // Start interval with getting current time every 1s
     if (countdownInterval) clearInterval(countdownInterval);
+
     countdownInterval = setInterval(
         () => {getTime()},
         1000
     );
 }
 
+/**
+ * @description Fill clock with start time values
+ *
+ * @param d days number
+ * @param h hours number
+ * @param m minutes number
+ * @param s seconds number
+ */
 function init_clock_digits(d, h, m, s) {
     const elements = {
         days: document.querySelectorAll('.days'),
@@ -44,23 +62,26 @@ function init_clock_digits(d, h, m, s) {
         seconds: document.querySelectorAll('.seconds')
     };
 
+    // Fill 4 clock columns - days, hours, minutes, seconds
+    // With 5 boxes in it - 1st (now - 2), 2nd (now - 2), 3rd (now), ...
+    // If no more days/hours/minutes/seconds, do now show next value
     for (let i = 0; i < elements.hours.length; i++) {
         if (d - 2 + i < 0) {
             elements.days[i].textContent = '';
         } else {
             elements.days[i].textContent = ((d - 2 + i)).toString().padStart(3, '0');
         }
-        if (h - 2 + i < 0 && d == 0) {
+        if (h - 2 + i < 0 && d === 0) {
             elements.hours[i].textContent = '';
         } else {
             elements.hours[i].textContent = ((h - 2 + i + 24) % 24).toString().padStart(2, '0');
         }
-        if (d == 0 && h == 0 && m - 2 + i < 0) {
+        if (d === 0 && h === 0 && m - 2 + i < 0) {
             elements.minutes[i].textContent = '';
         } else {
             elements.minutes[i].textContent = ((m - 2 + i + 60) % 60).toString().padStart(2, '0');
         }
-        if (d == 0 && h == 0 && m == 0 && s - 2 + i < 0) {
+        if (d === 0 && h === 0 && m === 0 && s - 2 + i < 0) {
             elements.seconds[i].textContent = ''
         } else {
             elements.seconds[i].textContent = ((s - 2 + i + 60) % 60).toString().padStart(2, '0');
@@ -68,6 +89,9 @@ function init_clock_digits(d, h, m, s) {
     }
 }
 
+/**
+ * @description Check current time
+ */
 function getTime() {
     const elements = {
         days: document.querySelectorAll('.days'),
@@ -79,25 +103,31 @@ function getTime() {
     const now = new Date();
     const diff = targetDate - now;
 
+    // Check if time is up
     if (diff <= 0) {
         clearInterval(countdownInterval);
 
-        document.getElementById('myModal').style.display = 'block';
-        document.getElementById('modalText').textContent = 'Time is up!';
+        document.getElementById('popup-container').style.display = 'block';
+        document.getElementById('popup-text').textContent = 'Time is up!';
 
-        var audio = new Audio('alarm.mp3');
+        // Alarm audio when time is up
+        let audio = new Audio('res/alarm.mp3');
         audio.play();
         
         return;
     }
 
+    // Get days, hours, minutes and seconds of difference
     const d = Math.floor(diff / 86400000);
-    const h = Math.floor(diff / 3600000);
+    const h = Math.floor((diff % 86400000) / 3600000);
     const m = Math.floor((diff % 3600000) / 60000);
     const s = Math.floor((diff % 60000) / 1000);
 
+    // 4 sliding animations (from 1st to 2nd element, from 2nd to 3rd element ... up to 5th)
     const animations = ['slideInTo50', 'slideInTo100', 'slideOutTo50', 'slideOutTo0'];
 
+    // Change time if the value has changed
+    // If no more days/hours/minutes/seconds, do now show next value
     if (d !== prev_days) {
         prev_days = d;
         for (let i = 0; i < elements.days.length; i++) {
@@ -111,29 +141,27 @@ function getTime() {
     if (h !== prev_hours) {
         prev_hours = h;
         for (let i = 0; i < elements.hours.length; i++) {
-            if (d == 0 && h - 2 + i < 0) {
+            if (d === 0 && h - 2 + i < 0) {
                 changeTime(elements.hours[i], '', animations[i], 2, '');
             } else {
                 changeTime(elements.hours[i], (h - 2 + i + 24) % 24, animations[i], 2, '0');
             }
         }
     }
-
     if (m !== prev_minutes) {
         prev_minutes = m;
         for (let i = 0; i < elements.minutes.length; i++) {
-            if (d == 0 && h == 0 && m - 2 + i < 0) {
+            if (d === 0 && h === 0 && m - 2 + i < 0) {
                 changeTime(elements.minutes[i], '', animations[i], 2, '')
             } else {
                 changeTime(elements.minutes[i], (m - 2 + i + 60) % 60, animations[i], 2, '0');
             }
         }
     }
-
     if (s !== prev_seconds) {
         prev_seconds = s;
         for (let i = 0; i < elements.seconds.length; i++) {
-            if (d == 0 && h == 0 && m == 0 && s - 2 + i < 0) {
+            if (d === 0 && h === 0 && m === 0 && s - 2 + i < 0) {
                 changeTime(elements.seconds[i], '', animations[i], 2, '')
             } else {
                 changeTime(elements.seconds[i], (s - 2 + i + 60) % 60, animations[i], 2, '0');
@@ -142,6 +170,18 @@ function getTime() {
     }
 }
 
+
+/**
+ * @description Play slide down animation for box
+ * When animation ends, box returns to its position and immediately changes value
+ * It gives illusion of smooth transition
+ *
+ * @param element document element
+ * @param value new value for an element (here number or empty string)
+ * @param animation animation to play
+ * @param pad target length for string.padStart here(2 or 3)
+ * @param fill string to fill for string.padStart (here 0 or empty string)
+ */
 function changeTime(element, value, animation, pad, fill) {
     element.classList.add(animation);
     element.addEventListener('animationend', function handler() {
--- scripts/datePicker.js
+++ scripts/datePicker.js
@@ -5,13 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const hourSelect = document.getElementById('hour-select');
     const minuteSelect = document.getElementById('minute-select');
     const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const currentDate = new Date().getDate();
 
+    // populate year section with next 10 years
     for (let year = currentYear; year <= currentYear + 10; year++) {
         const option = document.createElement('option');
-        option.value = year;
-        option.textContent = year;
+        option.value = year.toString();
+        option.textContent = year.toString();
         yearSelect.appendChild(option);
     }
 
@@ -20,29 +19,33 @@ document.addEventListener('DOMContentLoaded', function() {
         'May', 'June', 'July', 'August',
         'September', 'October', 'November', 'December'
     ]
+    // Populate month section with Jan - Dec
     for (let i = 0; i <= 11; i++) {
         const option = document.createElement('option');
-        option.value = i;
+        option.value = i.toString();
         option.textContent = monthsNames[i];
         monthSelect.appendChild(option);
     }
 
+    // We skip populating days section for now since amount of days depends on month
+
+    // Populate hour section with 00 - 23
     for (let i = 0; i <= 23; i++) {
         const option = document.createElement('option');
-        option.value = i;
+        option.value = i.toString();
         option.textContent = i.toString().padStart(2, '0');
         hourSelect.appendChild(option);
     }
 
+    // Populate minute section with 00 - 59
     for (let i = 0; i <= 59; i++) {
         const option = document.createElement('option');
-        option.value = i;
+        option.value = i.toString();
         option.textContent = i.toString().padStart(2, '0');
         minuteSelect.appendChild(option);
     }
 
-    
-
+    // Populate day section basing on selected month
     function populateDays() {
         const selectedYear = parseInt(yearSelect.value);
         const selectedMonth = parseInt(monthSelect.value);
@@ -50,19 +53,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
         daySelect.innerHTML = '';
 
+        // Populate day section with 1 - 28/29/30/31
         for (let day = 1; day <= daysInMonth; day++) {
             const option = document.createElement('option');
-            option.value = day;
-            option.textContent = day;
+            option.value = day.toString();
+            option.textContent = day.toString();
             daySelect.appendChild(option);
         }
     }
 
+    // Every time new year or month is selected, populate day section
     yearSelect.addEventListener('change', populateDays);
     monthSelect.addEventListener('change', populateDays);
 
+    // By default, select closest 30th of August
     let time;
-    if (new Date().getTime() < new Date(currentYear, 8, 30)) {
+
+    if (new Date().getTime() < new Date(currentYear, 7, 30)) {
         time = new Date(currentYear, 7, 30);
     } else {
         time = new Date(currentYear + 1, 7, 30);
@@ -77,7 +84,10 @@ document.addEventListener('DOMContentLoaded', function() {
     
 });
 
-function checkDate() {
+/**
+ * @description Check if selected date is in the future
+ */
+function validateDate() {
     const yearSelect = document.getElementById('year-select').value;
     const monthSelect = document.getElementById('month-select').value;
     const daySelect = document.getElementById('day-select').value;
@@ -86,26 +96,31 @@ function checkDate() {
     
     targetDate = new Date(yearSelect, monthSelect, daySelect, hourSelect, minuteSelect);
 
-    now = new Date()
+    let now = new Date()
 
     if (targetDate - now <= 0) {
-        var modal = document.getElementById('myModal');
-        modal.style.display = 'block';
+        // If past date, show pop-up
+        document.getElementById('popup-container').style.display = 'block';
+        document.getElementById('popup-text').textContent = 'Past date entered. Please try again.';
     } else {
+        // Else show clock counting down
         startCountdown();
     }
 }
 
-var span = document.getElementsByClassName('close')[0];
+// If event close button or window clicked, hide pop-up
+let span = document.getElementsByClassName('close-button')[0];
 
 span.onclick = function() {
-    var modal = document.getElementById('myModal');
-    modal.style.display = 'none';
+    let popup = document.getElementById('popup-container');
+
+    popup.style.display = 'none';
 }
 
 window.onclick = function(event) {
-    var modal = document.getElementById('myModal');
-    if (event.target == modal) {
-        modal.style.display = 'none';
+    let popup = document.getElementById('popup-container');
+
+    if (event.target === popup) {
+        popup.style.display = 'none';
     }
 }
